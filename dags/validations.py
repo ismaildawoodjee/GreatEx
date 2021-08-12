20518633f25054fe8cc4f80f98a8c4b3d0ebd46b
Ismail Dawoodjee <58870992+ismaildawoodjee@users.noreply.github.com>
2021-08-12 12:57:19 +0800

    Python script for sending STDOUT to Airflow logs

--- dags/validations.py
+++ dags/validations.py
@@ -1,56 +1,45 @@
-import os
-import sys
 import logging
 import subprocess
 from airflow.exceptions import AirflowException
 
 
-def validate_checkpoint_alt(checkpoint_name):
-    """Alternate method of validating checkpoints using `subprocess` and exceptions
+def validate_checkpoint(checkpoint_name):
+    """Python function to validate checkpoint. Writes output to Airflow logs and
+    also generates a list of Data Docs when the validation fails.
 
     Args:
         checkpoint_name (str): name of checkpoint to be validated against
 
     Raises:
-        AirflowException: when validation fails
+        AirflowException: when validation fails. Other ways of failing the validation
+        either make the Airflow Task incorrectly succeed, or completely fail
+        without being up for retry.
     """
-    try:
-        os.system("cd /opt/airflow")
-        result = subprocess.Popen(
-            ["great_expectations", "--v3-api", "checkpoint", "run", checkpoint_name]
-        )
-        text = result.communicate()[0]  # so that Bash exit code can be obtained
-        return_code = result.returncode
-        if return_code:
-            raise AirflowException(
-                "Checkpoint validation failed. The Bash command returned a non-zero exit code. "
-                "Inspect the Data Docs for more information.\n"
-            )
-    except AirflowException as error:
-        # use logging instead? with severity error. or try to exit with sys.exit
-        print(error)
-        subprocess.Popen(["great_expectations", "--v3-api", "docs", "list"])
-
-
-def validate_checkpoint(checkpoint_name):
-    import great_expectations as ge
-
-    context = ge.get_context()
-
-    result = context.run_checkpoint(
-        checkpoint_name=checkpoint_name,
-        batch_request=None,
-        run_name=None,
+    validation_process = subprocess.Popen(
+        ["great_expectations", "--v3-api", "checkpoint", "run", checkpoint_name],
+        stdout=subprocess.PIPE,
+        universal_newlines=True,
     )
+    validation_stdout = validation_process.communicate()[0]
+    return_code = validation_process.returncode
 
-    if not result["success"]:
-        logging.error("Validation failed! Inspect the Data Docs for more info")
-        os.system("great_expectations --v3-api docs list")
-        sys.exit(1)
+    if return_code:
+        logging.error(validation_stdout)
 
-    logging.info("Validation succeeded!")
-    sys.exit(0)
+        docs_process = subprocess.Popen(
+            ["great_expectations", "--v3-api", "docs", "list"],
+            stdout=subprocess.PIPE,
+            universal_newlines=True,
+        )
+        docs_stdout = docs_process.communicate()[0]
+        logging.info(docs_stdout)
+
+        raise AirflowException(
+            "Checkpoint validation failed. Inspect the Data Docs for more information."
+        )
+
+    else:
+        logging.info(validation_stdout)
 
 
-validate_checkpoint(checkpoint_name="retail_source_checkpoint")
-# validate_checkpoint_alt(checkpoint_name="retail_source_checkpoint")
+# validate_checkpoint(checkpoint_name="retail_source_checkpoint")
