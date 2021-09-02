d4d74b74dfaac9238df96f31412c658afd18f5be
Ismail Dawoodjee <58870992+ismaildawoodjee@users.noreply.github.com>
2021-09-02 16:09:47 +0800

    [BUGFIX] Ensure SQL statements to be idempotent

--- dags/scripts/sql/transform_load_retail_warehouse.sql
+++ dags/scripts/sql/transform_load_retail_warehouse.sql
@@ -1,4 +1,18 @@
--- final transformation before loading into production data warehouse
+-- Script to produce the final transformation before loading into the public/production data warehouse
+-- The INSERT operation must be idempotent, which means:
+-- the same INSERT operation must produce the same outcome, when running the same pipeline repeatedly
+-- To achieve this, the table must be dropped and recreated before inserting anything into it
+DROP TABLE IF EXISTS public.retail_profiling;
+
+CREATE TABLE public.retail_profiling (
+  customer_id INT,
+  stock_code VARCHAR(32),
+  invoice_date TIMESTAMP,
+  quantity INT,
+  unit_price NUMERIC(8, 3),
+  country VARCHAR(32)
+);
+
 INSERT INTO
   public.retail_profiling (
     customer_id,
