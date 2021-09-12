a701391d667a5f214639655ac0510e82f57e23fe
Ismail Dawoodjee <58870992+ismaildawoodjee@users.noreply.github.com>
2021-09-12 17:34:13 +0800

    [BUGFIX] Untrack  Python script from Git LFS

--- source-data/clean_trial_dataset.py
+++ source-data/clean_trial_dataset.py
@@ -1,3 +1,24 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:4ce97fb3a88ef190c49730de4a4ec66e9f07c1bc2b659d366dd92651bdf7b36e
-size 857
+"""Script for cleaning the trial dataset `retail_validating.csv` after
+renaming it to `retail_profiling.csv`. The original `retail_profiling.csv` file
+should be renamed to `retail_profiling_orig.csv`.
+"""
+import pandas as pd
+
+
+def clean_data():
+    df = pd.read_csv("retail_profiling.csv")
+
+    df["Quantity"] = [0 if qty < 0 else qty for qty in df["Quantity"]]
+    df["UnitPrice"] = [0.00 if price < 0 else price for price in df["UnitPrice"]]
+    # Casting CustomerID to integer so that it fits the INT type in the source database table
+    df["CustomerID"] = df["CustomerID"].astype("Int64")
+    df["Country"] = [
+        "United Kingdom" if country == "United KingdomKingdom" else country
+        for country in df["Country"]
+    ]
+
+    df.to_csv("retail_profiling.csv", index=False)
+
+
+if __name__ == "__main__":
+    clean_data()
